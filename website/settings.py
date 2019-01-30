3eb0896a49c37a3ee70518987fdccb0be1eea1dc
abdelrhman <abdelrhmansaeedbadr@gmail.com>
2019-01-30 03:52:04 -0800

    jk

--- website/settings.py
+++ website/settings.py
@@ -32,9 +32,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'yl5c57n!c!oja=mmr+m8a18w1#&b%g60ltmukw3=ezk*d62d9q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
+
 
 
 # Application definition
